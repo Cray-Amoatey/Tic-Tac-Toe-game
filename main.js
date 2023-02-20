f0e549a4737779568d2fb1e7e4401d96b3a86b8b
Amoatey <emmanuel.amoatey@amalitech.org>
2023-02-20 16:00:19 +0000

    created event listner for cpuBtn

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-
-
 // gameMenu
-const gameMenu = document.getElementById("gameMenu");
 const cpuBtn = document.getElementById("cpuBtn");
 const playerBtn = document.getElementById("playerBtn");
 
 // gameBoard
 const gameBoard = document.getElementById("gameBoard");
+const gameMenu = document.getElementById("gameMenu");
+
 const playerTurnIndicator = document.getElementById("playerturnIndicator");
 const restartGameBtn = document.querySelector(".restartGameBtn");
 const cellWrapper = document.getElementById("cellWrapper");
-const cell = document.querySelectorAll('.cell')
-
-
+const cell = document.querySelectorAll(".cell");
 
 // modal display
 const modal = document.getElementById("modal");
-const endGameModal = document.getElementById('endGameModal')
+const endGameModal = document.getElementById("endGameModal");
 const quitBtn = document.querySelector(".quitBtn");
 const nextroundBtn = document.querySelector(".nextBtn");
 
@@ -33,40 +30,11 @@ const scoreO = document.getElementById("player2Name");
 let PLAYER_X = '<img src= "assets/icon-x.svg">';
 let PLAYER_O = '<img src="assets/icon-o.svg">';
 
+const boardState = Array(cell.length);
+boardState.fill(null);
 
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
- 
-  
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cpuBtn.addEventListener("click", () => {
+  gameMenu.style.display = "none";
+  gameBoard.style.display = "initial";
+});
 
